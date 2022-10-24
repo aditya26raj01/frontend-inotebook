5ea6b843c79d11eea829c5412cc606c0973c72f9
Aditya <you@example.com>
2022-10-24 14:52:09 +0530

    setting up home about navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navbar from "../src/components/Navbar";
+import Home from "../src/components/Home";
+import About from "./components/About";
+
 function App() {
   return (
-    <div>App</div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route exact path="/about" element={<About/>} />
+        </Routes>
+      </Router>
+
+    </>
   );
 }
 
